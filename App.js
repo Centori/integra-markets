55dd4babe48d8042e501cd2b2b92f6331e05e242
Lani M <42298690+Centori@users.noreply.github.com>
2025-07-21 14:14:27 +0100

    Fix App.js import path

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 // Import the complete app from the app directory
 import React from 'react';
 import { registerRootComponent } from 'expo';
-import App from './app/app/(tabs)/index';
+import App from './app/App';
 
 // Register the main component
 registerRootComponent(App);
