48ddb316ced3b1942edddd09b34fa691abbcfe29
Lani M <42298690+Centori@users.noreply.github.com>
2025-07-21 14:16:30 +0100

    Switch to working TestApp component

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 // Import the complete app from the app directory
 import React from 'react';
 import { registerRootComponent } from 'expo';
-import App from './app/App';
+import App from './TestApp';
 
 // Register the main component
 registerRootComponent(App);
